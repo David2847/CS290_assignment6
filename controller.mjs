236857e65e5de695be2519ce529fd64458657e88
David Jantz <dljantz@gmail.com>
2024-11-12 22:11:40 -0600

    Finished!!

--- controller.mjs
+++ controller.mjs
@@ -35,19 +35,56 @@ app.post('/orders', (req, res) => {
 });
 
 app.get('/orders', (req, res) => {
-
+    if (req.query.company === undefined) {
+        // respond with all orders
+        const responseArray = model.getAllOrders();
+        res.status(200).json(responseArray);
+    } else {
+        // respond with all orders from specific company
+        const responseArray = model.getOrdersByCompany(req.query.company);
+        res.status(200).json(responseArray);
+    }
 });
 
 app.get('/orders/:id', (req, res) => {
-    
+    let orderObject = model.getOrderByID(req.params.id)
+    if (typeof orderObject === 'undefined') {
+        orderObject = {"Error": "Not found"};
+        res.status(404).json(orderObject);
+    } else {
+        res.status(200).json(orderObject);
+    }
 });
 
 app.put('/orders/:id', (req, res) => {
-    
+    const order = req.body;
+
+    // validate data:
+    //      company has at least two characters
+    //      quantity is integer greater than zero
+    if (Number.isInteger(order.quantity) && order.quantity > 0) {
+        const responseObject1 = model.getOrderByID(req.params.id);
+        if (typeof responseObject1 !== 'undefined') {
+            responseObject1["quantity"] = order.quantity;
+            model.replaceOrderByID(req.params.id, responseObject1);
+            res.status(201).json(responseObject1);
+            return;
+        }
+        const responseObject2 = {"Error": "Not found"};
+        res.status(400).json(responseObject2);
+        return;
+    }
+    const responseObject3 = {"Error": "Invalid request"};
+    res.status(400).json(responseObject3);
 });
 
 app.delete('/orders/:id', (req, res) => {
-    
+    if (model.deleteOrderByID(req.params.id)) {
+        res.status(204).send();
+        return;
+    };
+    const responseObject = {"Error": "Not found"};
+    res.status(404).json(responseObject);
 });
 
 // Don't change or add anything below this line
--- model.mjs
+++ model.mjs
@@ -16,8 +16,54 @@ function createOrder(company, quantity){
 
 // Don't change anything above this line
 
-function displayModel() {
-    console.log(orders);
+// returns JSON array of all orders
+function getAllOrders() {
+    return orders;
 }
 
-export {createOrder, displayModel};
+// returns JSON array of orders from a particular company
+function getOrdersByCompany(company) {
+    const companyOrders = [];
+    for (let i=0; i<orders.length; i++) {
+        if (orders[i]["company"] === company) {
+            companyOrders.push(orders[i]);
+        }
+    }
+    return companyOrders;
+}
+
+function getOrderByID(id) {
+    for (let i=0; i<orders.length; i++) {
+        if (orders[i]["id"] === id) {
+            return orders[i];
+        }
+    }
+}
+
+function replaceOrderByID(id, order) {
+    for (let i=0; i<orders.length; i++) {
+        if (orders[i]["id"] === id) {
+            // replace order
+            orders[i] = order;
+            return true;
+        }
+    }
+    return false;
+}
+
+function deleteOrderByID(id, order) {
+    for (let i=0; i<orders.length; i++) {
+        if (orders[i]["id"] === id) {
+            orders.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
+}
+
+export {createOrder,
+        getOrdersByCompany,
+        getAllOrders,
+        getOrderByID,
+        replaceOrderByID,
+        deleteOrderByID};
